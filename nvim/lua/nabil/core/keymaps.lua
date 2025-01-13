2c88b8101a66bc64e2db7bc1d8385a0d9044ac70
Nabil080 <bellilanabil8@gmail.com>
2025-01-13 14:57:58 +0100

    added mason and lsp config

--- nvim/lua/nabil/core/keymaps.lua
+++ nvim/lua/nabil/core/keymaps.lua
@@ -21,3 +21,9 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- arrow keys
+keymap.set("n", "<Left>", '<cmd>echo "Stop using arrow keys"<CR>')
+keymap.set("n", "<Right>", '<cmd>echo "Stop using arrow keys"<CR>')
+keymap.set("n", "<Up>", '<cmd>echo "Stop using arrow keys"<CR>')
+keymap.set("n", "<Down>", '<cmd>echo "Stop using arrow keys"<CR>')
