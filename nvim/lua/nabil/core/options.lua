2ceb573e90bd438225cdc47a343a4ba9db719945
Nabil080 <nbellila.student@42.fr>
2025-01-14 10:55:41 +0100

    added scroll offset and highlight on yank

--- nvim/lua/nabil/core/options.lua
+++ nvim/lua/nabil/core/options.lua
@@ -37,3 +37,17 @@ opt.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
 opt.swapfile = false
+
+-- scroll offset
+opt.so = 40
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
