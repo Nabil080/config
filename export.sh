fd40b123e0ab6714c26529d0a084b4fc11df61a7
Nabil080 <bellilanabil8@gmail.com>
2025-01-28 19:16:21 +0100

    export script done, might want to install neovim dependencies in the script (lazygit etc)

--- export.sh
+++ export.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Exports the current config inside the home
+# /!\ This script usings apt as a package manager
 
 is_installed()
 {
@@ -10,29 +11,75 @@ is_installed()
     fi
 }
 
-# zsh
-# ensure oh my zsh is installed
-if is_installed zsh; then
-    echo 'zsh is installed'
-else
-    echo 'zsh is not installed'
+dir_exists()
+{
+    if [ -d $1 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# NOTE: zsh config
+if ! is_installed zsh; then
+    # installs zsh
+    read -p "Zsh is not installed, do you want to install it ? (y/n)" yn
+    case $yn in
+        [Yy]* ) sudo apt install zsh;;
+    esac
 fi
-cp ./config.zsh $HOME/.oh-my-zsh/custom 
+if ! dir_exists ${HOME}/.oh-my-zsh; then
+    # installs oh my zsh
+    read -p "Oh-My-Zsh is not installed, do you want to install it ? (y/n)" yn
+    case $yn in
+        [Yy]* ) sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    esac
+fi
+if is_installed zsh && dir_exists $HOME/.oh-my-zsh ; then
+    echo "Exporting oh-my-zsh config"
+    cp ./config.zsh $HOME/.oh-my-zsh/custom 
+fi
 
 
-# tmux
-# ensure tmux is installed
-# clone plugin manager
-cp ./.tmux.conf $HOME
+# NOTE: tmux
+if ! is_installed tmux; then
+    # installs tmux
+    read -p "Tmux is not installed, do you want to install it ? (y/n)" yn
+    case $yn in
+        [Yy]* ) sudo apt install tmux;;
+    esac
+fi
+if is_installed tmux; then
+    echo "Exporting tmux config"
+    cp ./.tmux.conf $HOME
+    if ! dir_exists $HOME/.tmux/plugins/tpm; then
+        echo "Installing tmux plugin manager"
+        git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+    fi
+    echo "Don't forget to install plugins using prefix+I inside tmux"
+fi
 
-# nvim
-# ensure nvim is installed
-# ensure necessary dependencies are installed
-# # lazygit
-# # ripgrep
-# # nerd font
-# # clang
-# # python
-cp -r nvim $HOME/.config
+# NOTE: nvim
+if ! is_installed nvim; then
+    # installs nvim
+    read -p "Neovim is not installed, do you want to install it ? (y/n)" yn
+    case $yn in
+        [Yy]* ) sudo apt-get install neovim;;
+    esac
+fi
+if is_installed nvim; then
+    echo "Exporting nvim config"
+    cp -r nvim $HOME/.config
+    echo "Don't forget to install the dependencies :
+    - A nerdfont for icons
+    - Lazygit
+    - ripgrep for the fuzzy finder
+    - python and node
+    - lua
+    - clang
+    and others idk"
+    # might want to install dependencies
+fi
+
 
 cp ./.clang-format $HOME
